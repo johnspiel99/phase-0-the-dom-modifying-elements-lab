5008430f81c640cb1f430520ddddda689e2e6709
Johnspiel <njuejohnspiel@gmail.com>
2024-06-24 08:45:46 +0300

    New

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+
+const main = document.getElementById('main');
+if (main) {
+    main.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; 
+
+
+document.body.appendChild(newHeader);
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
